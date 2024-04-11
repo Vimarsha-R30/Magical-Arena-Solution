c43bc57b7af4cc3b94152570bb43e87db2975d48
Vimarsha-R30 <vimarsha3005@gmail.com>
2024-04-11 16:34:48 +0530

    Defining players

--- src/MagicalArena.java
+++ src/MagicalArena.java
@@ -1,2 +1,9 @@
 public class MagicalArena {
+    public Player playerA;
+    public Player playerB;
+    public MagicalArena(Player playerA, Player playerB) {
+        this.playerA = playerA;
+        this.playerB = playerB;
+    }
+
 }
