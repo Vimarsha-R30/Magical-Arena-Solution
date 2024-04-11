0959ffe500fa2f5a11c12d6d3e040a96a0fb9663
Vimarsha-R30 <vimarsha3005@gmail.com>
2024-04-11 16:32:44 +0530

    Creating Player class

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,23 @@
 public class Player {
+    int health;
+    int strength;
+    int attack;
+
+    public Player(int health, int strength, int attack) {
+        this.health = health;
+        this.strength = strength;
+        this.attack = attack;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
 }
