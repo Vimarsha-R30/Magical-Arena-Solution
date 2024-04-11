d29213e267909e8629de535540731cd7dd0a7dc4
Vimarsha-R30 <vimarsha3005@gmail.com>
2024-04-11 16:51:37 +0530

    Calling fight method to start the game

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         Player playerA = new Player(50,5,10);
         Player playerB = new Player(100,10,5);
         MagicalArena arena = new MagicalArena(playerA, playerB);
+        arena.fight();
     }
 }
