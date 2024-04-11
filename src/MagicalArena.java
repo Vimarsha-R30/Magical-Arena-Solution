9b70143df780c30f69e60783ba0f046c0e040399
Vimarsha-R30 <vimarsha3005@gmail.com>
2024-04-11 16:41:03 +0530

    Defining game logic

--- src/MagicalArena.java
+++ src/MagicalArena.java
@@ -5,5 +5,13 @@ public class MagicalArena {
         this.playerA = playerA;
         this.playerB = playerB;
     }
+    public void fight() {
+        Player attacker = playerA.getHealth() < playerB.getHealth() ? playerA : playerB;
+        Player defender = attacker == playerA ? playerB : playerA;
 
+        while (playerA.isAlive() && playerB.isAlive()){
+            int attackRoll = attacker.rollDie();
+            int defendRoll = defender.rollDie();
+        }
+    }
 }
